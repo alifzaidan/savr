9c6961f1dea2d5af3f2708ff22eba5e71ee82215
alifzaidan <zaidanalif22710@gmail.com>
2025-01-09 17:53:31 +0700

    add chart to ui

--- app/(dashboard)/analytics/page.tsx
+++ app/(dashboard)/analytics/page.tsx
@@ -1,5 +1,8 @@
+import { AnalyticsChart } from '@/components/layout/dashboard/AnalyticsChart';
 import DetailAnalytics from '@/components/layout/dashboard/DetailAnalytics';
 import { Badge } from '@/components/ui/badge';
+import { Button } from '@/components/ui/button';
+import { Card } from '@/components/ui/card';
 import { Progress } from '@/components/ui/progress';
 import { Separator } from '@/components/ui/separator';
 import { ChevronUp } from 'lucide-react';
@@ -10,25 +13,31 @@ export default function page() {
             <h1 className="font-amstelvar text-3xl mb-2 ml-4">Analytics</h1>
             <p className="mb-6 opacity-80 text-sm ml-4">Get insights into your spending habits and financial health.</p>
 
-            <div className="w-full bg-sidebar flex flex-col px-4 py-6 rounded-xl border border-border mb-6">
-                <div className="w-full bg-background p-4 border border-border rounded-xl">
-                    <div className="bg-sidebar p-4 border border-border rounded-xl">
-                        <p className="text-sm text-muted-foreground mb-1">Overview</p>
-                        <h3 className="font-amstelvar text-2xl">Rp. 1.000.000</h3>
-                        <div className="flex gap-2 items-center mt-3">
-                            <Badge variant={'default'}>
-                                <ChevronUp size={12} className="mr-1" strokeWidth={4} />
-                                10%
-                            </Badge>
-                            <p className="text-xs text-muted-foreground mb-1">Last 10 days</p>
+            <div className="w-full bg-sidebar flex flex-col px-6 py-6 rounded-xl border border-border mb-6">
+                <div className="w-full flex gap-6 bg-background p-6 border border-border rounded-xl">
+                    <Card className="flex flex-col gap-4 p-6 border border-border rounded-xl">
+                        <div className="flex-grow">
+                            <p className="text-sm text-muted-foreground mb-1">Overview</p>
+                            <h3 className="font-amstelvar text-2xl">Rp. 1.000.000</h3>
+                            <div className="flex gap-2 items-center mt-3">
+                                <Badge variant={'default'}>
+                                    <ChevronUp size={12} className="mr-1" strokeWidth={4} />
+                                    10%
+                                </Badge>
+                                <p className="text-xs text-muted-foreground mb-1">Last 10 days</p>
+                            </div>
+                            <Progress value={10} className="my-6" />
+                            <div>
+                                <DetailAnalytics title="Gajian" amount={1000000} percentage={10} />
+                                <Separator className="my-4" />
+                                <DetailAnalytics title="THR" amount={1000000} percentage={10} />
+                            </div>
                         </div>
-                        <Progress value={10} className="my-6" />
-                        <div>
-                            <DetailAnalytics title="Gajian" amount={1000000} percentage={10} />
-                            <Separator className="my-4" />
-                            <DetailAnalytics title="THR" amount={1000000} percentage={10} />
-                        </div>
-                    </div>
+                        <Button variant="secondary" className="w-full">
+                            Edit Limits
+                        </Button>
+                    </Card>
+                    <AnalyticsChart />
                 </div>
             </div>
         </section>
--- components/layout/dashboard/DetailAnalytics.tsx
+++ components/layout/dashboard/DetailAnalytics.tsx
@@ -1,13 +1,19 @@
 export default function DetailAnalytics({ title, amount, percentage }: { title: string; amount: number; percentage: number }) {
+    const formattedAmount = new Intl.NumberFormat('id-ID', {
+        style: 'currency',
+        currency: 'IDR',
+        minimumFractionDigits: 0,
+        maximumFractionDigits: 0,
+    }).format(amount);
     return (
-        <div className="flex justify-between">
+        <div className="flex justify-between gap-16">
             <div className="flex items-center gap-2">
                 <div className="h-8 w-8 rounded-full bg-blue-600"></div>
-                <p className="font-medium">{title}</p>
+                <p className="text-sm font-medium">{title}</p>
             </div>
             <div className="text-right">
-                <p className="font-medium">{amount}</p>
-                <p className="text-sm text-muted-foreground">{percentage}%</p>
+                <p className="text-sm font-medium">{formattedAmount}</p>
+                <p className="text-xs text-muted-foreground">{percentage}%</p>
             </div>
         </div>
     );
